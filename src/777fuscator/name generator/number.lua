bb3bbcce5ac28c482674c55df2d0ad0bcbf2118b
aUxcDeVs <msnsmsstbrostuxk@gmail.com>
2025-10-05 22:18:24 +0800

    Delete src/777fuscator/name generator/number.lua

--- src/777fuscator/name generator/number.lua
+++
@@ -1,9 +0,0 @@
--- 777Fuscator Name Generator: Number
--- Made by Claude & 777Fuscator Team
--- Simple hexadecimal-based name generation
-
-local PREFIX = "_0x";
-
-return function(id, scope)
-	return PREFIX .. string.format("%x", id);
-end
