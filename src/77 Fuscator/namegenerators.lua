0b3e410dff497b99e22586b9708c2bfa181ef0e6
aUxcDeVs <msnsmsstbrostuxk@gmail.com>
2025-10-05 22:39:30 +0800

    Create namegenerators.lua

---
+++ src/77 Fuscator/namegenerators.lua
@@ -0,0 +1,7 @@
+return {
+	Mangled = require("777Fuscator.Obfuscator.src.namegenerator.mangled");
+	MangledShuffled = require("777Fuscator.Obfuscator.src.namegenerator.mangled_shuffled");
+	Il = require("777Fuscator.Obfuscator.src.namegenerator.Il");
+	Number = require("777Fuscator.Obfuscator.src.namegenerator.number");
+	Confuse = require("777Fuscator.Obfuscator.src.namegenerator.confuse");
+}
