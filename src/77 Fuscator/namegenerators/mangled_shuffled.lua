ce4ab1b53e4ef0e9b3632b6863f774b1b41ee0f8
aUxcDeVs <msnsmsstbrostuxk@gmail.com>
2025-10-05 22:30:32 +0800

    Create mangled_shuffled.lua

---
+++ src/77 Fuscator/namegenerators/mangled_shuffled.lua
@@ -0,0 +1,34 @@
+-- 777Fuscator Name Generator: Mangled Shuffled
+-- Made by Claude & 777Fuscator Team
+-- Mangled names with randomized character order
+
+local util = require("777Fuscator.util");
+local chararray = util.chararray;
+
+local VarDigits = chararray("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_");
+local VarStartDigits = chararray("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+
+local function generateName(id, scope)
+	local name = ''
+	local d = id % #VarStartDigits
+	id = (id - d) / #VarStartDigits
+	name = name .. VarStartDigits[d + 1]
+	
+	while id > 0 do
+		local d = id % #VarDigits
+		id = (id - d) / #VarDigits
+		name = name .. VarDigits[d + 1]
+	end
+	
+	return name
+end
+
+local function prepare(ast)
+	util.shuffle(VarDigits);
+	util.shuffle(VarStartDigits);
+end
+
+return {
+	generateName = generateName,
+	prepare = prepare
+}
